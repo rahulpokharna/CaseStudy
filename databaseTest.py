82f94043e53d1a0329c9a567cca934626492e08c
Rahul <pepsi1423@gmail.com>
2017-10-20 12:25:42 -0400

    Edited dbtest for creation of the db.

--- databaseTest.py
+++ databaseTest.py
@@ -4,14 +4,14 @@ from sqlite3 import Error
 #File path/name of database file to be read
 DATABASE = 'new.db'
 
-#Connection to the database
-conn = sqlite3.connect(DATABASE)
-c = conn.cursor()
+
 
 
 def initDB(db_file):
     """ Initialize the database with the specified tables and other information """
-    
+    #Connection to the database
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
     try:
 
         '''initialize the Tables'''
@@ -20,16 +20,20 @@ def initDB(db_file):
         #c.execute('''CREATE TABLE user(UserID integer, email text, FirstName text, LastName text, HashedPassword text, GoogleID text, CanvasID text, DriveLink text)''')
         
         '''Add Test Values to Tables'''
-        #c.execute("INSERT INTO user VALUES (0001'abc123@case.edu','Alpha', 'Cavern','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
-        #c.execute("INSERT INTO user VALUES (0002'axc1223@case.edu','Andrew', 'Clark','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
-        #c.execute("INSERT INTO user VALUES (0003,'yxs123@case.edu','Yongju', 'Sui','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
-        
-        c.execute("INSERT INTO event VALUES (0002, 0001,'Start Time', 'End Time','this is annoying',4,'Test Event 2', -1,'One Time', '','')")
+        '''
+        c.execute("INSERT INTO user VALUES (0001,'abc123@case.edu','Alpha', 'Cavern','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
+        c.execute("INSERT INTO user VALUES (0002,'axc1223@case.edu','Andrew', 'Clark','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
+        c.execute("INSERT INTO user VALUES (0003,'yxs123@case.edu','Yongju', 'Sui','hashed','','','https://drive.google.com/open?id=0B8WM6XnQ3RJ6RS1XUzNfLVNnQlU')")
+        '''
+        #c.execute("INSERT INTO event VALUES (0002, 0001,'Start Time', 'End Time','this is annoying',4,'Test Event 2', -1,'One Time', '','')")
         
         '''Test Queries'''
-        #for row in c.execute("SELECT email FROM user"):
-        #    print(row)
         
+        #test for printing
+        for row in c.execute("SELECT * FROM user"):
+            for col in row:
+                print(col)
+            print()
         
 
 
@@ -38,25 +42,8 @@ def initDB(db_file):
     except Error as e:
         print(e)
     finally:
-        
- 
-def getEvent(givenID):
-    
-    #need to edit later, perhaps create a 'global' c value, as defined above
-     return c.execute("SELECT * FROM event WHERE eventID = '%s'" % givenID)
-
-def addNewEvent(EventID, UserID, Time, Length, Date, Description, ImportanceRanking, Title, ProgramID , EventType, StudyPlan, StudyType):
-    # make sure all information is in the correct formats. Date and Time processing can be done here.
-    #format can be copied for all tables
-    try:
-        print('success')
-        conn.commit()
-    except Error as e:
-        print(e)
-
-    finally:
+        conn.close()
         
 
 if __name__ == '__main__':
     initDB(DATABASE)
-    conn.close()
