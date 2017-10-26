5ce64d388ba741e7347c2ee28a193ae4af694bf9
Sibi Sengottuvel <sibivel979@gmail.com>
2017-10-25 21:19:10 -0400

    working on making request methods for get and post events

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response, jsonify
 from login import LoginForm
+from dbRequests import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -25,6 +26,23 @@ def login():
         else:
             print(str(request.form['email']))
             print(str(request.form['password']))
+
+# for get request request with userId or eventId as url parameters (put it after the url like:
+# localhost://5000/requests/events?userID=1
+@app.route('/request/events', methods=['GET','POST'])
+def requestEvent():
+
+    #request with userID or eventID given as url parameters. If eventId is not given, this will return all events for this user.
+    if request.method == 'GET':
+        userID = request.args.get('userID')
+        eventID = request.args.get('eventID')
+        if eventID == None:
+            return jsonify(getUserEvents(userID))
+        else:
+            return jsonify(getEvent(eventID))
+
+    
+
 
 @app.route('/home')
 def home():
